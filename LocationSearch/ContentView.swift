acb5691505875c57c397f32d0312cbe94c73f2c1
Peter Alt <peter@peteralt.com>
2020-06-30 13:19:20 -0600

    Initial Commit

---
+++ LocationSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocationSearch
+//
+//  Created by Peter Alt on 6/30/20.
+//  Copyright © 2020 Peter Alt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
