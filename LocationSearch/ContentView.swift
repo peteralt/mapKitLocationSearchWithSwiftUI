6387c358a997fa1715ac9678268f89d21f0e567f
Peter Alt <peter@peteralt.com>
2020-06-30 13:58:41 -0600

    Demo implementation

--- LocationSearch/ContentView.swift
+++ LocationSearch/ContentView.swift
@@ -8,14 +8,41 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @ObservedObject var locationService: LocationService
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Form {
+                Section(header: Text("Location Search")) {
+                    ZStack(alignment: .trailing) {
+                        TextField("Search", text: $locationService.queryFragment)
+                        if locationService.status == .isSearching {
+                            Image(systemName: "clock")
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                }
+                Section(header: Text("Results")) {
+                    List {
+                        if locationService.status == .noResults {
+                            Text("No Results")
+                                .foregroundColor(Color.gray)
+                        }
+                        ForEach(locationService.searchResults, id: \.self) { completionResult in
+                            Text(completionResult.title)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(locationService: LocationService())
     }
 }
---
+++ LocationSearch/LocationService.swift
@@ -0,0 +1,59 @@
+//
+//  LocationService.swift
+//  LocationSearch
+//
+//  Created by Peter Alt on 6/30/20.
+//  Copyright © 2020 Peter Alt. All rights reserved.
+//
+
+import Foundation
+import Combine
+import MapKit
+
+class LocationService: NSObject, ObservableObject {
+    
+    enum LocationStatus: Equatable {
+        case idle
+        case noResults
+        case isSearching
+        case error(String?)
+        case result
+    }
+    
+    @Published var queryFragment: String = ""
+    @Published private(set) var status: LocationStatus = .idle
+    @Published private(set) var searchResults: [MKLocalSearchCompletion] = []
+    
+    private var queryCancellable: AnyCancellable?
+    private let searchCompleter: MKLocalSearchCompleter!
+    
+    init(searchCompleter: MKLocalSearchCompleter = MKLocalSearchCompleter()) {
+        self.searchCompleter = searchCompleter
+        super.init()
+        self.searchCompleter.delegate = self
+        
+        queryCancellable = $queryFragment
+            .receive(on: DispatchQueue.main)
+            .debounce(for: .milliseconds(250), scheduler: RunLoop.main, options: nil)
+            .sink(receiveValue: { fragment in
+                self.status = .isSearching
+                if !fragment.isEmpty {
+                    self.searchCompleter.queryFragment = fragment
+                } else {
+                    self.status = .idle
+                    self.searchResults = []
+                }
+        })
+    }
+}
+
+extension LocationService: MKLocalSearchCompleterDelegate {
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        self.searchResults = completer.results.filter({ $0.subtitle == "" })
+        self.status = completer.results.isEmpty ? .noResults : .result
+    }
+    
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        self.status = .error(error.localizedDescription)
+    }
+}
